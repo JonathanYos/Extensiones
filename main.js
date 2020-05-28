25e26e8f43418457ee5b22f6ec5ccd0ce3915417
Jonathan Yos <jonathnayos16@gmail.com>
2020-05-28 16:48:38 -0600

    Implemetar Javascript

---
+++ main.js
@@ -0,0 +1,328 @@
+ var sistemas=[
+    "Jonathan Yos-213",
+    "Brandon Barrera-212",
+    "Javier Estrada-211",
+    "Francisco B.-211",
+    "Lizandro C. CDI-618",
+    "Sevidores-120"
+]; 
+
+llenartabla(sistemas,"#tbsist","Patricia de León","210")
+
+var conta=[
+"Edwin Girón-402",
+"Fredy Paniagua-405",
+"Jose Luis V.-402",
+"Marcos Castro-409",
+"Victor M.Caja-403"
+];
+llenartabla(conta,"#tbconta","Claudia Silva","404");
+
+var rh =[
+    "Consultor RH-407"
+]
+
+llenartabla(rh,"#tbrh","Claudia H.","408");
+
+var apad=[
+"Aracely Pérez-103",
+"Carmen García-103",
+"Fax General-104",
+"Isabel Romero-102",
+"Lys Barrios-102",
+"Marissa V.-101"
+];
+
+llenartabla(apad,"#tbapad","Rosalina Ramos","513");
+
+var oper=[
+"Miguel G.(Bodega)-106",
+"Cocina-512",
+"Mario L-201",
+"Martín Zamora-652",
+"Luis G.-652"
+];
+
+llenartabla(oper,"#tbope","Josué Sícan","511");
+
+var hosp=[
+    "Casa Vol.-525",
+    "Emily S.-126",
+    "Erin Gyori-126"
+]
+
+llenartabla(hosp,"#tbhosp","Erin Treinen","128");
+
+var recau=[
+    "Hospitalidad-153"
+];
+
+llenartabla(recau,"#tbrecau","Verena M.","315");
+
+var varios=[
+    "Fax General-104",
+    "Recep. Clínica-301",
+    "Recepción General-101",
+    "Sala A(AE)-206",
+    "Sala B(AE)-207",
+    "Sala Emergen.-302",
+    "Sala Hospitalidad-127",
+    "Salón Confern.-320",
+    "Salón Rosas-112"
+]
+
+llenartabla(varios,"#tbvarios","Fax Clínica","311");
+
+var sucur=[
+    "Brenda Realique-222",
+    "Educ.Sucursales-221",
+    "Emma García-222",
+    "Ligia Pol-221",
+    "María Ramírez-222",
+    "Promotores-221"
+]
+
+llenartablasucur(sucur,"#tbsucur","Alma Marroquín-220","Sofía Díaz-222","Mirna Boteo-222");
+
+var secu=[
+    "Oficina Seguridad-204"
+];
+
+llenartabla(secu,"#tbsecu","Entrada Principal","110");
+var jov=[
+"Azucena Barrientos-610",
+"Jaime Guerra-611",
+"Paulo García-609"
+];
+
+llenartabla(jov,"#tbjov","Rigoberto García","608");
+
+var educ=[
+"Angélica Pumay-616",
+"Jorge Monge-616",
+"Anímate-617"
+];
+
+llenartabla(educ,"#tbeduc","Patricia Gómez","603");
+
+var apoy=[
+"Angel Cubur-205",
+"Alejandra Jocop-205",
+"EPS-205"
+];
+
+llenartabla(apoy,"#tbapoy","Felipe Bagur","205");
+
+var becas=[
+"Becas-614",
+"Luis Pichillá-615",
+"Nydia Gonzáles-619"
+];
+
+llenartabla(becas,"#tbbecas","Nineth García","620");
+
+var vivienda=[
+"Edwin C.-651",
+"Fernando Santos-651",
+"Reynaldo-651"
+];
+
+llenartabla(vivienda,"#tbvivenda","Josué Sícan","511");
+
+var ci=[
+    "Azucena Tecun-606",
+    "Elizabeth Gonzáles-606",
+    "Nilsy Pérez-606",
+    "Carmen Gil-606",
+    "Salón Bébes-604"
+];
+
+llenartabla(ci,"#tbci","Helen Jerez","606");
+
+var salud=[
+"Laboratorio-309",
+"Clínica-310",
+"Clínica Dental-316",
+"Farmacia Ana Alonzo-401",
+"Signos Vitales-302",
+"Michelle(Recepción)-301"
+];
+
+llenartabla(salud,"#tbsalud","Laura Gonzáles","304");
+
+var bibli=[
+"Berta B.-602",
+"Gricelda-602"
+];
+
+llenartabla(bibli,"#tbbibli","Lynn Miller","602");
+
+var direc=[
+"Rebecca Sanborn-312-Directora Nacional",
+"Pablo Cermeño-202-Director Administrativo",
+"Jeff Barnes-502-Paneación Estratégica",
+"Renato Westby-526-Programas Educativos"
+];
+
+llenartabladirec(direc,"#tbdirec");
+
+var ts1=[
+    "Beatriz López-124",
+    "Gabriela Velásquez-122",
+    "Jazmin González-124"
+];
+
+var ts2=[
+    "Lissgrid López-124",
+    "Lydia J.-121",
+    "Paola de Matta-124",
+    "Romelia Porón-121",
+    "Rosa Cano-123"
+];
+
+llenartablasucur(ts1,"#tbts1","Patricia Ramírez-108","Adriana Alquijay-122","Alicia Ramírez-123")
+
+llenartabla(ts2,"#tbts2","Lesbia García","123");
+
+var celulares=[
+'Alicia Ramírez+5482-7378',
+'Claudia Silva+5482-7376',
+'Guardianes+5482-7396',
+'Luis Pichillá+4770-4383',
+'Patricia Ramírez+5482-7412',
+'Sofia Díaz+5951-5984',	
+'Alicia Ramírez (ts)+5482-7381',
+'Coord. Brigadas+5482-7419',
+'Jaime Guerra+4210-1811',
+'Luis Velázquez+5482-7384',
+'Pablo Cermeño+5482-7375',
+'Alma Marroquín+4112-9801',
+'Daniel Gómez+5554-4035',
+'Jeff Barnes+5482-7418',
+'Lys Barrios+5482-7386',
+'Rebecca Sanborn+5923-2058',
+'Ana Zuleta+5951-6002',
+'Edwin Calí+4770-2413',
+'Hospitalidad+5482-7387',
+'Mario (Piloto)+5693-4707',
+'Renato Westby+5482-7397',
+'Beatriz López+5482-7374',
+'Erin Treinen+4264-9205',
+'Jonathan de León+4770-3407',
+'Mirna Boteo+5482-7365',
+'Recursos Humanos+5482-7382',
+'Beatriz López (ts)+5843-2190',
+'Elsa García+5120-4323',
+'Josué Sicán+5482-7362',
+'Nineth García+5951-6013',		
+'Rigoberto García+5951-5997',	
+'Carmen García+5482-7361',
+'Fernando Santos+5482-7371',
+'Leslie Murga+5482-7392',
+'Patricia de León+4229-4581',
+'Rosalina Ramos+5482-7338',
+'Gerente NE+5482-5449',
+'Ligia Pol+5923-7155',
+'Patricia Gómez+5951-6003',
+'Salud+5826-9791',
+];
+llenarcelulares(celulares);
+var w1=$("#tbcel1").width();
+var w2=$("#tbcel2").width();
+var w3=$("#tbcel3").width();
+var w4=$("#tbcel4").width();
+var w5=$("#tbcel5").width();
+var w6=$("#tbcel6").width();
+var sumador=0;
+sumador=sumador+w1+2;
+$("#tbcel2").css("left",sumador+"px");
+sumador=sumador+w2+2;
+$("#tbcel3").css("left",sumador+"px");
+sumador=sumador+w3+2;
+$("#tbcel4").css("left",sumador+"px");
+sumador=sumador+w4+2;
+$("#tbcel5").css("left",sumador+"px");
+sumador=sumador+w5+2;
+$("#tbcel6").css("left",sumador+"px");
+sumador=sumador+w6+4;
+$("#celular").css("width",sumador+"px")
+function llenarcelulares(array){
+    $("#tbcel1").html("");
+    $("#tbcel2").html("");
+    $("#tbcel3").html("");
+    $("#tbcel4").html("");
+    $("#tbcel5").html("");
+    $("#tbcel6").html("");
+    array.sort();
+    var conteo=0;
+array.forEach(soli => {
+    if(conteo>=0&&conteo<=7){
+    separarcel(soli,"#tbcel1");
+    }
+    if(conteo>=8&&conteo<=15){
+    separarcel(soli,"#tbcel2");
+    }
+    if(conteo>=16&&conteo<=23){
+    separarcel(soli,"#tbcel3");
+    }
+    if(conteo>=24&&conteo<=31){
+    separarcel(soli,"#tbcel4");
+    }
+    if(conteo>=32&&conteo<=39){
+    separarcel(soli,"#tbcel5");
+    }
+    if(conteo>=40&&conteo<=47){
+    separarcel(soli,"#tbcel6");
+    }
+    conteo++;
+});
+}
+
+function llenartabla(array,tabla,nombre,ext){
+$(tabla).html("");
+array.sort();
+var trger='<tr><td>'+nombre+'</td><td><b>'+ext+'<b></td></tr>';
+$(tabla).append(trger);
+array.forEach(soli => {
+separar(soli,tabla);
+});
+}
+function llenartabladirec(array,tabla){
+    $(tabla).html("");
+    array.forEach(soli => {
+    separardirec(soli,tabla);
+    });
+}
+function llenartablasucur(array,tabla,nombre1,nombre2,nombre3){
+    $(tabla).html("");
+    array.sort();
+    separar(nombre1,tabla);
+    separar(nombre2,tabla);
+    separar(nombre3,tabla);
+    array.forEach(soli => {
+    separar(soli,tabla);
+    });
+    
+}
+function separar(linea,tabla){
+    var separacion = linea.split('-');
+    var nombre = separacion[0];
+    var extension=separacion[1];
+    var tr='<tr><td>'+nombre+'</td><td><b>'+extension+'<b></td></tr>';
+        $(tabla).append(tr);
+}  
+function separardirec(linea,tabla){
+    var separacion = linea.split('-');
+    var nombre = separacion[0];
+    var extension=separacion[1];
+    var puesto = separacion[2];
+    var tr='<tr><td>'+nombre+'</td><td>'+puesto+'<td><td><b>'+extension+'<b></td></tr>';
+        $(tabla).append(tr);
+}
+function separarcel(linea,tabla){
+    var separacion = linea.split('+');
+    var nombre = separacion[0];
+    var extension=separacion[1];
+    var tr='<tr><td>'+nombre+'</td><td><b>'+extension+'<b></td></tr>';
+    $(tabla).append(tr);
+}
